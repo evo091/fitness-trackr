9bc1be86e41ccd9eefcd580957fe4a9ffeab5b54
Ian <236912195+evo091@users.noreply.github.com>
2026-03-23 10:56:10 -0400

    added error message to tell user they must be the activity creator to delete an activity

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,27 +1,49 @@
+import { useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 import { deleteActivity } from "../api/activities";
 
 export default function ActivityList({ activities, syncActivities }) {
 
   const { token } = useAuth();
+  const [error, setError] = useState();
+
+    const handleDelete = async (activity) => {
+    setError(null);
+    try {
+      await deleteActivity(token, activity);
+      syncActivities();
+    } catch (e) {
+      setError(e.message);
+    }
+  };
 
   if (!token) {
-    return (
-        <ul>
-      {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
-      ))}
-    </ul>
-    )
+        return (
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
+    );
   } else {
     return (
-    <ul>
-      {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}
-            <button id="delete" className="delete-button" onClick={ async () =>{await deleteActivity(token, activity); syncActivities()}}>X</button>
-        </li>
-      ))}
-    </ul>
-  );
+      <>
+        {error && <p role="alert">{error}</p>}
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>
+              {activity.name}
+              <button
+                id="delete"
+                className="delete-button"
+                onClick={() => handleDelete(activity)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+    </>
+    );
   }
 }
